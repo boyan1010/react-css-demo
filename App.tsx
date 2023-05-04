59ed74b5b20793b37db13f0f5eca5ed25375c492
boyan1010 <846184637@qq.com>
2023-05-04 08:48:17 +0800

    background-clip

--- App.tsx
+++ App.tsx
@@ -8,10 +8,10 @@ export default function App() {
       background-clip： 用于设置背景的延伸区域
       border/padding/content-box：延伸至border/padding/content-box的外边框内
       默认为border-box
-      <div className="wrapper" style="background: ">
-        <div className="border-box"></div>
-        <div className="content-box"></div>
-        <div className="padding-box"></div>
+      <div className="wrapper">
+        <div className="border-box box"></div>
+        <div className="content-box box"></div>
+        <div className="padding-box box"></div>
       </div>
     </div>
   );
